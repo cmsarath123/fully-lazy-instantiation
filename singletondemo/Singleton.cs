3e3eb287f595453e88734e8f652799a8fbf695d6
sarathkumarc <cmsarath123@gmail.com>
2020-08-15 21:09:39 +0530

    singleton demo

---
+++ singletondemo/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace singletondemo
+{
+    public class Singleton
+    {
+        private static int ObjectsCount = 0;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return Nested.instance;
+            }
+        }
+
+        private class Nested
+        {
+            internal static readonly Singleton instance = new Singleton();
+            static Nested()
+            {
+            }
+        }
+
+        private Singleton()
+        {
+            ObjectsCount++;
+            Console.WriteLine($"instances {ObjectsCount}");
+        }
+
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
